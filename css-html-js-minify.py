c1d5faa0c44031c17b99baf237fe55d1f9dbabba
juancarlospaco <juancarlospaco@gmail.com>
2015-10-22 03:14:09 -0300

    Clean out

--- css-html-js-minify.py
+++ css-html-js-minify.py
@@ -16,6 +16,7 @@ import re
 import socket
 import sys
 import traceback
+
 from argparse import ArgumentParser
 from copy import copy
 from ctypes import byref, cdll, create_string_buffer
@@ -1145,46 +1146,6 @@ def check_working_folder(folder_to_check=os.path.expanduser("~")):
     return False
 
 
-def log_exception():
-    """Log Exceptions but pretty printing with more info, return string."""
-    unfriendly_names = {"<module>": "Unnamed Anonymous Module Function",
-                        "<stdin>": "System Standard Input Function"}
-    line_tpl = "    |___ {key} = {val}  # Type: {t}, Size: {s}Bytes, ID: {i}\n"
-    body_tpl = """
-    ################################ D E B U G ###############################
-    Listing all Local objects by context frame, ordered by innermost last:
-    {body}
-    Thats all we know about the error, check the LOG file and StdOut.
-    ############################### D E B U G #############################"""
-    tb, body_txt, whole_txt = sys.exc_info()[2], "", ""
-    while 1:
-        if not tb.tb_next:
-            break
-        tb = tb.tb_next
-    stack = []
-    f = tb.tb_frame
-    while f:
-        stack.append(f)
-        f = f.f_back
-    stack.reverse()
-    traceback.print_exc()
-    for frame in stack:
-        if frame.f_code.co_name in unfriendly_names.keys():
-            fun = unfriendly_names[frame.f_code.co_name]
-        else:
-            fun = "Function {0}()".format(frame.f_code.co_name)
-        body_txt += "\nThe {nm} from file {fl} at line {ln} failed!.".format(
-            nm=fun, fl=frame.f_code.co_filename, ln=frame.f_lineno)
-        body_txt += "\n    {}\n    |\n".format(fun)
-        for key, value in frame.f_locals.items():
-            whole_txt += line_tpl.format(key=key, val=repr(value)[:50],
-                                         t=str(type(value))[:25],
-                                         s=sys.getsizeof(key), i=id(value))
-    result = body_tpl.format(body=body_txt + whole_txt)
-    log.debug(result)
-    return result
-
-
 def make_root_check_and_encoding_debug():
     """Debug and Log Encodings and Check for root/administrator,return Boolean.
 
@@ -1383,14 +1344,13 @@ def main():
     log.disable(log.CRITICAL) if args.quiet else log.debug("Max Logging ON")
     log.info(__doc__ + __version__)
     check_working_folder(os.path.dirname(args.fullpath))
-    # Work based on if argument is file or folder, folder is slower.
     if os.path.isfile(args.fullpath) and args.fullpath.endswith(".css"):
-        log.info("Target is a CSS File.")
-        list_of_files = str(args.fullpath)
+        log.info("Target is a CSS File.")  #  Work based on if argument is
+        list_of_files = str(args.fullpath)  # file or folder, folder is slower.
         process_single_css_file(args.fullpath)
     elif os.path.isfile(args.fullpath) and args.fullpath.endswith(
             ".html" if args.overwrite else ".htm"):
-        log.info("Target is HTM{} File.".format("L" if args.overwrite else ""))
+        log.info("Target is HTML File.")
         list_of_files = str(args.fullpath)
         process_single_html_file(args.fullpath)
     elif os.path.isfile(args.fullpath) and args.fullpath.endswith(".js"):
@@ -1398,8 +1358,7 @@ def main():
         list_of_files = str(args.fullpath)
         process_single_js_file(args.fullpath)
     elif os.path.isdir(args.fullpath):
-        log.info("Target is a Folder with CSS, HTM{}, JS files !.".format(
-            "L" if args.overwrite else ""))
+        log.info("Target is a Folder with CSS, HTML, JS files !.")
         log.warning("Processing a whole Folder may take some time...")
         list_of_files = walkdir_to_filelist(
             args.fullpath,
